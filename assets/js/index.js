a3c1b255abefe0e922c3c1e30a91a751fe8cc37d
johnnyferreiradev <johnnyferreiradevweb@gmail.com>
2020-08-05 21:01:25 -0300

    Adjusting button style

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,52 +2,43 @@
 const browser = document.querySelector('.browser');
 
 const urlElement = browser.querySelector('#url_text');
-const tabs = browser.querySelectorAll('.tab');
-
 const frameTab = browser.querySelector('.responsive-iframe');
+const closeButton = browser.querySelector('.dot');
 
 // states
-const registeredDomains = [
-    'www.paradigm.com',
-    'www.thereal.com',
-];
+
 
 // functions
-const deactiveTab = (tab) => {
-    tabs.forEach((tab) => tab.classList.remove('active'));
+const removeElement = (element) => {
+    element.parentNode.removeElement(element);
 }
 
-const toggleActiveTab = (e) => {
-    const tab = e.target;
-    deactiveTab(tab);
-    console.log(tab.classList.add('active'));
-};
+const closeWindow = () => {
+    console.log('Clicou');
+}
+
+const createIframe = (url) => {
+    const iframe = document.createElement('iframe');
+    iframe.name = 'frameTab';
+    iframe.className = 'responsive-iframe';
+    iframe.src = url;
+
+    return iframe;
+}
 
 const fetchUrl = (url) => {
-    frameTab.src = `./${url}`;
+    frameTab.src = url;
 }
 
-// www.paradigm.com
-// www.thereal.com
 const search = (e) => {
     const urlElement = e.target;
     const key = e.which || e.keyCode;
 
     if (key === 13) {
-        const isRegistered = registeredDomains.find((url) => url === urlElement.value);
-
-        if (isRegistered) {
-            fetchUrl(urlElement.value);
-        } else {
-            console.log('passou aqui');
-            fetchUrl('not-found');
-        }
+        fetchUrl(urlElement.value);
     }
 }
 
 // event assignment
 urlElement.addEventListener('keyup', search);
-
-tabs.forEach((tab) => {
-    tab.addEventListener('click', toggleActiveTab);
-});
+closeButton.addEventListener('click', closeWindow);
